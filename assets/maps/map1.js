1fda1b47f02bb923f16bd7cb03dcbfd5cf5d3f7b
William J Riley <bill.riley@gmail.com>
2025-02-14 14:59:18 -0500

    removing a coujple of bricks from map1 to make it more playable

--- assets/maps/map1.js
+++ assets/maps/map1.js
@@ -1,38 +1,38 @@
 const map1 = {
-    "name": "Level 1",    
-    "playerStartingPosition": {x: 50, y: 60},
-    "mapBackgroundColor": "#87CEEB",
-    "music": "./assets/music/2019-01-02_-_8_Bit_Menu_-_David_Renda_-_FesliyanStudios.com.mp3",
-    "mapData": [        
-        "!                                                                                     #",        
-        "!                                                                                     #",        
-        "!                                                                                     #",                                
-        "!                                                                                     #",        
-        "!                                                                                     #", 
-        "!                                                                                     #",
-        "!                                                                                     #",
-        "!                                                                                     #",                
-        "!  $                                                                                  #",
-        "!           @   $                                                                     #",
-        "!   ####    ########                                                                  #",
-        "!           ###    #                                                                  #",
-        "!       @    ##   $#    ###                                                           #",
-        "!  #  #####      ##                          ##########                               #",
-        "!       $                      ###########################                            #",
-        "!        $    #       #########!!!!!!!!!!!!!!!!!!!!!!###########                      #",
-        "!                    ##!!!!!!!!!!!!!!!!!!!!!!!!!!!!!################                  0",
-        "!        ####    @  ########!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!##########              0",
-        "#######################################################################################"
-    ],
-    "tileDefinitions": {
-      "#": { "type": "solid", "color": "gray", "image": "./assets/dirt.png" },
-      "!": { "type": "solid", "color": "darkgray", "image": "./assets/dirt2.png" },
-      "@": { "type": "enemy", "color": "red", "image": "./assets/enemy1.png" },
-      "$": { "type": "collectible", "color": "yellow", "image": "./assets/coin.png" },
-      "0": { "type": "loadMap", "script": "map2", "color": "purple" },
-      "P": { "type": "passable", "color": "lightgray" }      
-    }    
-  };
-  
+  "name": "Level 1",
+  "playerStartingPosition": { x: 50, y: 60 },
+  "mapBackgroundColor": "#87CEEB",
+  "music": "./assets/music/2019-01-02_-_8_Bit_Menu_-_David_Renda_-_FesliyanStudios.com.mp3",
+  "mapData": [
+    "!                                                                                     #",
+    "!                                                                                     #",
+    "!                                                                                     #",
+    "!                                                                                     #",
+    "!                                                                                     #",
+    "!                                                                                     #",
+    "!                                                                                     #",
+    "!                                                                                     #",
+    "!  $                                                                                  #",
+    "!           @   $                                                                     #",
+    "!   ####    ########                                                                  #",
+    "!           ###    #                                                                  #",
+    "!       @         $#    ###                                                           #",
+    "!  #  #####      ##                          ##########                               #",
+    "!       $                      ###########################                            #",
+    "!        $    #       #########!!!!!!!!!!!!!!!!!!!!!!###########                      #",
+    "!                    ##!!!!!!!!!!!!!!!!!!!!!!!!!!!!!################                  0",
+    "!        ####    @  ########!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!##########              0",
+    "#######################################################################################"
+  ],
+  "tileDefinitions": {
+    "#": { "type": "solid", "color": "gray", "image": "./assets/dirt.png" },
+    "!": { "type": "solid", "color": "darkgray", "image": "./assets/dirt2.png" },
+    "@": { "type": "enemy", "color": "red", "image": "./assets/enemy1.png" },
+    "$": { "type": "collectible", "color": "yellow", "image": "./assets/coin.png" },
+    "0": { "type": "loadMap", "script": "map2", "color": "purple" },
+    "P": { "type": "passable", "color": "lightgray" }
+  }
+};
 
-  window.map1 = map1;
+
+window.map1 = map1;
