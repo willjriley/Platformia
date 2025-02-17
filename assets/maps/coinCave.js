e53f6f01184ef553d551685c13d8ccb0479a34f2
William J Riley <bill.riley@gmail.com>
2025-02-17 12:44:28 -0500

    bats and springs oh my!

--- assets/maps/coinCave.js
+++ assets/maps/coinCave.js
@@ -1,30 +1,30 @@
 const coinCave = {
     "name": "coinCave",
-    "playerStartingPosition": { x: 50, y: 80 },
+    "playerStartingPosition": { x: 50, y: 40 },
     "mapBackgroundColor": "#000000",
     "gradientTop": "#000000", // Dark teal
     "gradientMiddle": "#003300", // Dark green
     "gradientBottom": "#004d4d", // Almost black    
     "mapData": [
-        "#########################################################################",
-        "#                                  ##                         0~        #",
-        "#                                  ##                         ~~ $      #",
-        "#                                  ##                         #####     #",
-        "##############################     ##   $                               #",
-        "#                             #    #   $ $                              #",
-        "#   $                         #  $ #  $   $            $$               #",
-        "#   $ $                       #     ###########       #####             #",
-        "#   $ $                         $                                       #",
-        "#   $                                                               S   #",
-        "#   $                            $                            ###########",
-        "#            $ $                                                        #",
-        "#                                $               S                      #",
-        "#                                        ###########                    #",
-        "#   S   $          $   $                                                #",
-        "#   ######       #########      ######       $ $ $ $                    #",
-        "#                                          $ $ $ $ $ $ $                #",
-        "# $  $       $           $     $       $                                #",
-        "#########################################################################"
+        "###################################################################################",
+        "#                                            ##         ^               0~        #",
+        "#                                            ##                         ~~ $      #",
+        "#########################################    ##                         #####     #",
+        "#                                      ##    ##   $                               #",
+        "#                                      ##    #   $ $                              #",
+        "#             $                        ##  $ #  $   $            $$               #",
+        "#             $ $                      ##     ###########       #####             #",
+        "#      ###   $ $   ###                    $     ^                                 #",
+        "#       ^     $     ^                                                         S   #",
+        "#             $                            $                            ###########",
+        "#                      $ $                                                        #",
+        "#                                          $               S                      #",
+        "#                                                  ###########                    #",
+        "#             S   $          $   $                                                #",
+        "#         ##########       #########      ######       $ $ $ $                    #",
+        "#                                                    $ $ $ $ $ $ $                #",
+        "#           $  $       $           $     $       $                                #",
+        "###################################################################################"
     ],
     "tileDefinitions": {
         "#": { "type": "solid", "color": "#4B0082", "image": "./assets/metalBox.png" },
@@ -32,7 +32,8 @@ const coinCave = {
         "$": { "type": "collectible", "color": "yellow", "image": "./assets/coin.png" },
         "S": { "type": "bounce", "image": "./assets/spring.png", "force": 12 },
         "0": { "type": "loadMap", "script": "map1", "width": 64, "height": 64, "image": "./assets/door.png" },
-        "~": { "type": "filler", "note:": "filler for the map editor, i do nothing but show the occupied space for the door" }
+        "~": { "type": "filler", "note:": "filler for the map editor, i do nothing but show the occupied space for the door" },
+        "^": { "type": "enemy", "color": "red", "image": "./assets/bat.png", "enemyType": "hunter" }
     }
 };
 window.coinCave = coinCave
