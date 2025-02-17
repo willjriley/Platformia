6cb35581f605d2b6025c6ff9296a05c9f2184e08
William J Riley <bill.riley@gmail.com>
2025-02-17 12:02:40 -0500

    small adjustment for lavaCaverns - looks better.

--- assets/maps/lavaCaverns.js
+++ assets/maps/lavaCaverns.js
@@ -21,16 +21,17 @@ const lavaCaverns = {
         "#                                           ##############################################################                                 #",
         "#                  M     M                 ##############################################################################                  #",
         "#                 MGMLLLMGM   S            #################################################################################################",
-        "############################################################################################################################################"
+        "#####################BBB####################################################################################################################"
     ],
     "tileDefinitions": {
         "#": { "type": "solid", "color": "#4B0082", "image": "./assets/red_tile.png" },
         "M": { "type": "solid", "color": "#4B0082", "image": "./assets/metalBox.png" },
         "G": { "type": "solid", "color": "#1A1A1A" },
+        "B": { "type": "solid", "color": "#000000" },
         "@": { "type": "enemy", "color": "red", "image": "./assets/enemy1.png" },
         "$": { "type": "collectible", "color": "yellow", "image": "./assets/coin.png" },
         "0": { "type": "loadMap", "script": "map1" },
-        "L": { "type": "loadMap", "color": "#ff0000", "script": "coinCave" },
+        "L": { "type": "loadMap", "color": "#000000", "script": "coinCave" },
         "S": { "type": "bounce", "image": "./assets/spring.png", "force": 16 },
         "0": { "type": "loadMap", "script": "map1", "width": 64, "height": 64, "image": "./assets/door.png" },
         "~": { "type": "filler", "note:": "filler for the map editor, i do nothing but show the occupied space for the door" },
