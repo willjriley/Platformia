2b1be83dbbd3b69a4ef7c7a4765cf26f8a7972e1
William J Riley <bill.riley@gmail.com>
2025-02-16 23:53:30 -0500

    merge

--- assets/maps/coinCave.js
+++ assets/maps/coinCave.js
@@ -30,7 +30,7 @@ const coinCave = {
         "#": { "type": "solid", "color": "#4B0082", "image": "./assets/metalBox.png" },
         "@": { "type": "enemy", "color": "red", "image": "./assets/enemy1.png" },
         "$": { "type": "collectible", "color": "yellow", "image": "./assets/coin.png" },
-        "S": { "type": "bounce", "image": "./assets/spring.png", "force": 15 },
+        "S": { "type": "bounce", "image": "./assets/spring.png", "force": 12 },
         "0": { "type": "loadMap", "script": "map1", "width": 64, "height": 64, "image": "./assets/door.png" },
         "~": { "type": "filler", "note:": "filler for the map editor, i do nothing but show the occupied space for the door" }
     }
--- game.js
+++ game.js
@@ -201,14 +201,6 @@ function isColliding(player, platform) {
     return collidesHorizontally && collidesVertically;
 }
 
-
-// function isColliding(obj1, obj2) {
-//     return obj1.x + obj1.width > obj2.x &&
-//         obj1.x < obj2.x + obj2.width &&
-//         obj1.y + obj1.height > obj2.y &&
-//         obj1.y < obj2.y + obj2.height;
-// }
-
 function displayYouDiedMessage() {
     ctx.fillStyle = "red";
     ctx.font = "bold 50px 'Courier New', monospace";
